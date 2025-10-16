ff2160a4ae288faa0922e00ae4c9c32ee3d347dc
Santiago Poveda <santypoveda1112@gmail.com>
2025-10-16 16:47:05 -0500

    Agrega ruta de login al react-router

--- test-onboarding/src/App.tsx
+++ test-onboarding/src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Register from './componets/register-index/register.tsx';
+import Login from './componets/login-index/login.tsx';
 
 function App() {
   return (
     <BrowserRouter> 
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter> //
   );//
