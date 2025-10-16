67528b6ce46ce02c2e1687f16ad60fa1f67df92e
Santiago Poveda <santypoveda1112@gmail.com>
2025-10-16 16:46:21 -0500

    Creación de la pestaña de login

---
+++ test-onboarding/src/componets/login-index/login.tsx
@@ -0,0 +1,103 @@
+import img1 from "../../assets/img-1.png";
+import facebookIcon from "../../assets/Facebook.png";
+import appleIcon from "../../assets/apple.png";
+import googleIcon from "../../assets/google.png";
+import compensar from "../../assets/Group 1.png";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./login.css";
+
+function Login() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const form = event.currentTarget;
+    const acount = form.elements[0] as HTMLInputElement;
+    const password = form.elements[1] as HTMLInputElement;
+    setLoading(true);
+    setError("");
+
+    const usersApi = fetch(
+      `https://7wmbjxblzi.execute-api.us-east-1.amazonaws.com/login`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          emailOrUser: acount.value,
+          password: password.value,
+        }),
+      }
+    );
+    usersApi.then((resp) => {
+      resp.json().then((data) => {
+        console.log(data);
+        if (resp.status === 200) {
+          navigate("/survey");
+          sessionStorage.setItem("user", data.data.user);
+        } else {
+          setError("Usuario o contraseña incorrectos");
+          setLoading(false);
+        }
+      });
+    });
+
+    event.preventDefault();
+  };
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="login-container">
+      <img src={compensar} alt="Icono de compensar" className="icono_login" />
+      <div className="login-left">
+        <h1>Bienvenido</h1>
+        <p className="text">Ingresa y disfruta</p>
+        <p className="info">
+          Si aún no tienes una cuenta <br /> puedes{" "}
+          <a href="/register" className="links">
+            Registrarte aquí!
+          </a>
+        </p>
+        <img src={img1} alt="Ilustración de bienvenida" className="img-1" />
+      </div>
+
+      <div className="login-right">
+        <form onSubmit={handleSubmit}>
+          <h2>Iniciar sesión</h2>
+          <input
+            type="email"
+            placeholder="Email o nombre de usuario"
+            required
+          />
+          <input type="password" placeholder="Contraseña" required />
+          <div className="login-options">
+            <a href="" className="link_olvidar">
+              Olvide mi contraseña
+            </a>
+          </div>
+          <button className="btm-login" type="submit" disabled={loading}>
+            {loading ? "Cargando..." : "Iniciar sesión"}
+          </button>
+          {error && <p className="error">{error}</p>}
+          <div className="social-login">
+            <p>o continua con</p>
+            <a href="">
+              <img src={facebookIcon} alt="Facebook" />
+            </a>
+            <a href="">
+              <img src={appleIcon} alt="Apple" />
+            </a>
+            <a href="">
+              <img src={googleIcon} alt="Google" />
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
