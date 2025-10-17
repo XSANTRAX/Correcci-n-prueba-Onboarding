8bd407795a84de7e32c2ca577ef792aac018ef95
Santiago Poveda <santypoveda1112@gmail.com>
2025-10-16 23:44:54 -0500

    Implementación del componente Skeletor Survey

--- test-onboarding/src/componets/survey-index/survey.tsx
+++ test-onboarding/src/componets/survey-index/survey.tsx
@@ -1,11 +1,13 @@
 import compensar from "../../assets/Group 1.png";
 import confirm from "../../assets/Illustration.png";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import SurveySkeleton from "./Skeletor-survey";
 import "./Survey.css";
 
 function Survey() {
   const [sunmitting, setSubmitting] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -68,117 +70,129 @@ function Survey() {
     }
   }
 
-  return (
-    <div className="survey">
-      <img src={compensar} alt="Icono_de_compensar" className="icono_survey" />
-      <div className="diseño">
-        <form onSubmit={handleSubmit}>
-          <h1>Encuesta</h1>
-          <label htmlFor="fecha">Fecha:</label>
-          <input type="date" id="fecha" name="fecha" required />
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
+  if (loading) {
+    return SurveySkeleton();
+  } else {
+    return (
+      <div className="survey">
+        <img
+          src={compensar}
+          alt="Icono_de_compensar"
+          className="icono_survey"
+        />
+        <div className="diseño">
+          <form onSubmit={handleSubmit}>
+            <h1>Encuesta</h1>
+            <label htmlFor="fecha">Fecha:</label>
+            <input type="date" id="fecha" name="fecha" required />
 
-          <label htmlFor="pregunta1">Pregunta 1</label>
-          <div className="opciones">
-            <div>
-              <input type="radio" name="tipo1" id="A1" value="A" required />
-              <label htmlFor="A1">A</label>
+            <label htmlFor="pregunta1">Pregunta 1</label>
+            <div className="opciones">
+              <div>
+                <input type="radio" name="tipo1" id="A1" value="A" required />
+                <label htmlFor="A1">A</label>
+              </div>
+              <div>
+                <input type="radio" name="tipo1" id="B1" value="B" required />
+                <label htmlFor="B1">B</label>
+              </div>
+              <div>
+                <input type="radio" name="tipo1" id="C1" value="C" required />
+                <label htmlFor="C1">C</label>
+              </div>
+              <div>
+                <input type="radio" name="tipo1" id="D1" value="D" required />
+                <label htmlFor="D1">D</label>
+              </div>
             </div>
-            <div>
-              <input type="radio" name="tipo1" id="B1" value="B" required />
-              <label htmlFor="B1">B</label>
-            </div>
-            <div>
-              <input type="radio" name="tipo1" id="C1" value="C" required />
-              <label htmlFor="C1">C</label>
-            </div>
-            <div>
-              <input type="radio" name="tipo1" id="D1" value="D" required />
-              <label htmlFor="D1">D</label>
-            </div>
-          </div>
 
-          <label htmlFor="pregunta2">Pregunta 2</label>
-          <div className="opciones">
-            <div>
-              <input type="radio" name="tipo2" id="A2" value="A" required />
-              <label htmlFor="A2">A</label>
+            <label htmlFor="pregunta2">Pregunta 2</label>
+            <div className="opciones">
+              <div>
+                <input type="radio" name="tipo2" id="A2" value="A" required />
+                <label htmlFor="A2">A</label>
+              </div>
+              <div>
+                <input type="radio" name="tipo2" id="B2" value="B" required />
+                <label htmlFor="B2">B</label>
+              </div>
+              <div>
+                <input type="radio" name="tipo2" id="C2" value="C" required />
+                <label htmlFor="C2">C</label>
+              </div>
+              <div>
+                <input type="radio" name="tipo2" id="D2" value="D" required />
+                <label htmlFor="D2">D</label>
+              </div>
             </div>
-            <div>
-              <input type="radio" name="tipo2" id="B2" value="B" required />
-              <label htmlFor="B2">B</label>
-            </div>
-            <div>
-              <input type="radio" name="tipo2" id="C2" value="C" required />
-              <label htmlFor="C2">C</label>
-            </div>
-            <div>
-              <input type="radio" name="tipo2" id="D2" value="D" required />
-              <label htmlFor="D2">D</label>
-            </div>
-          </div>
 
-          <label htmlFor="pregunta3">Pregunta 3</label>
-          <div className="opciones">
-            <div>
-              <input type="radio" name="tipo3" id="A3" value="A" required />
-              <label htmlFor="A3">A</label>
+            <label htmlFor="pregunta3">Pregunta 3</label>
+            <div className="opciones">
+              <div>
+                <input type="radio" name="tipo3" id="A3" value="A" required />
+                <label htmlFor="A3">A</label>
+              </div>
+              <div>
+                <input type="radio" name="tipo3" id="B3" value="B" required />
+                <label htmlFor="B3">B</label>
+              </div>
+              <div>
+                <input type="radio" name="tipo3" id="C3" value="C" required />
+                <label htmlFor="C3">C</label>
+              </div>
+              <div>
+                <input type="radio" name="tipo3" id="D3" value="D" required />
+                <label htmlFor="D3">D</label>
+              </div>
             </div>
-            <div>
-              <input type="radio" name="tipo3" id="B3" value="B" required />
-              <label htmlFor="B3">B</label>
-            </div>
-            <div>
-              <input type="radio" name="tipo3" id="C3" value="C" required />
-              <label htmlFor="C3">C</label>
-            </div>
-            <div>
-              <input type="radio" name="tipo3" id="D3" value="D" required />
-              <label htmlFor="D3">D</label>
-            </div>
-          </div>
 
-          <label htmlFor="pregunta4">Pregunta 4</label>
-          <div className="opciones">
-            <div>
-              <input type="radio" name="tipo4" id="A4" value="A" required />
-              <label htmlFor="A4">A</label>
+            <label htmlFor="pregunta4">Pregunta 4</label>
+            <div className="opciones">
+              <div>
+                <input type="radio" name="tipo4" id="A4" value="A" required />
+                <label htmlFor="A4">A</label>
+              </div>
+              <div>
+                <input type="radio" name="tipo4" id="B4" value="B" required />
+                <label htmlFor="B4">B</label>
+              </div>
+              <div>
+                <input type="radio" name="tipo4" id="C4" value="C" required />
+                <label htmlFor="C4">C</label>
+              </div>
+              <div>
+                <input type="radio" name="tipo4" id="D4" value="D" required />
+                <label htmlFor="D4">D</label>
+              </div>
             </div>
-            <div>
-              <input type="radio" name="tipo4" id="B4" value="B" required />
-              <label htmlFor="B4">B</label>
-            </div>
-            <div>
-              <input type="radio" name="tipo4" id="C4" value="C" required />
-              <label htmlFor="C4">C</label>
-            </div>
-            <div>
-              <input type="radio" name="tipo4" id="D4" value="D" required />
-              <label htmlFor="D4">D</label>
-            </div>
-          </div>
 
-          <div className="btm-contenedor">
-            <button
-              className="btm-survey"
-              type="submit"
-              value="enviar"
-              id="btnenviar"
-              disabled={sunmitting}
-            >
-              {sunmitting ? <div className="spinner" /> : "Enviar Encuesta"}
-            </button>
+            <div className="btm-contenedor">
+              <button
+                className="btm-survey"
+                type="submit"
+                value="enviar"
+                id="btnenviar"
+                disabled={sunmitting}
+              >
+                {sunmitting ? <div className="spinner" /> : "Enviar Encuesta"}
+              </button>
+            </div>
+          </form>
+          <div className="confirmacion" id="confirmacion">
+            <div className="icono">
+              <img src={confirm} alt="Icono de confirmacion" />
+            </div>
+            <p>Tus respuestas se han guardado de forma correcta</p>
+            <button onClick={cerrarConfirmacion}>Siguiente</button>
           </div>
-        </form>
-        <div className="confirmacion" id="confirmacion">
-          <div className="icono">
-            <img src={confirm} alt="Icono de confirmacion" />
-          </div>
-          <p>Tus respuestas se han guardado de forma correcta</p>
-          <button onClick={cerrarConfirmacion}>Siguiente</button>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default Survey;
